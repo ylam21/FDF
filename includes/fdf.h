6de6fad51d48365309eef20ddbbfeaae60b7eb4c
ylam21 <ohnivasasanka@gmail.com>
2025-10-29 20:21:53 +0100

    check for .fdf extension

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: omaly <omaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 19:20:43 by omaly             #+#    #+#             */
-/*   Updated: 2025/10/18 19:50:23 by omaly            ###   ########.fr       */
+/*   Updated: 2025/10/29 20:14:10 by omaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,15 @@
 #include "../lib/mlx/mlx.h"
 #include "../lib/mlx/mlx_int.h"
 
+#include <fcntl.h>
+
 // Window size for mlx_new_window()
 #define WINDOW_SIZE_X 1920
 #define WINDOW_SIZE_Y 1080
+
+
+typedef struct s_fdf {
+	int file;
+} t_fdf;
 
 #endif
--- src/init_fdf.c
+++ src/init_fdf.c
@@ -1,23 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fdf.c                                              :+:      :+:    :+:   */
+/*   init_fdf.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: omaly <omaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/18 19:35:52 by omaly             #+#    #+#             */
-/*   Updated: 2025/10/29 20:19:07 by omaly            ###   ########.fr       */
+/*   Created: 2025/10/29 19:51:52 by omaly             #+#    #+#             */
+/*   Updated: 2025/10/29 20:18:32 by omaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-int main(int argc, char **argv)
+int open_map(char *map)
 {
-	t_fdf fdf;
-	if (init_fdf(fdf, argc, argv) != 0) {
+	int fd;
+	if (!map)
 		return 1;
+	else if (ft_strnstr(map, ".fdf", 4) == NULL) {
+		ft_putstr_fd("no .fdf extension\n",2);
+		return 2;
 	}
+	fd = open(map, O_RDONLY);
+	if (fd < 0) {
+		perror("open");
+		return 3;
+	}
+	return fd;
+}
 
-	return (EXIT_SUCCESS);
+int init_fdf(t_fdf fdf, int argc, char **argv)
+{
+	if (argc != 2)
+		return 1;
+	fdf.file = open_map(argv[1]);
+	if (fdf.file != 0)
+		return 2;
+	return 0;
 }
